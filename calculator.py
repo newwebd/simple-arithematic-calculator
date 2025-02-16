f75ce739d3d9a78cd3713a8710f2e868d47ec734
newwebd <164335157+newwebd@users.noreply.github.com>
2025-02-16 07:39:00 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+cal1=int(input('    <..      ')) 
+opration=input('    ..     ') 
+cal2=int(input('    ..>      '))
+conca=cal1,opration,cal2
+if conca=='+':
+    print(f'= {cal1 + cal2} ')
+if opration=='-':
+        print(f' = { cal1-cal2}')
+if opration=='*':
+              print(f'= {cal1 * cal2}')
+if opration =='/':
+        print(f' ={cal1 / cal2}')
+elif opration =='%':
+        print(f' = {cal1 % cal2}')
+
+
+
+
+
